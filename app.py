9206dcf66e96db225a3e9f73b75dea78ffbdc525
looooll2007-collab <looooll2007@gmail.com>
2026-08-06 05:38:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -259,7 +259,7 @@ with st.form("login_form", border=False):
     login_button = st.form_submit_button("تسجيل الدخول", use_container_width=True)
 
 # تنفيذ التنسيق والتحقق أول ما يضغط Enter أو الماوس
-if if login_button:
+ if login_button:
     # كود التحقق من الباسورد بتاعك:
     if username == "admin" and password == "123456": # عدل شروطك هنا
         st.success("تم تسجيل الدخول بنجاح!")
